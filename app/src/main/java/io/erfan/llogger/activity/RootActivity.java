b0c33461cfee900931a8576fff368bf8bf37c96f
atrotors <dev@atrotors.com>
2017-06-08 16:37:27 +1000

    convert to bottom nav

--- app/src/main/java/io/erfan/llogger/activity/RootActivity.java
+++ app/src/main/java/io/erfan/llogger/activity/RootActivity.java
@@ -1,22 +1,17 @@
 package io.erfan.llogger.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import io.erfan.llogger.R;
 
-public class RootActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class RootActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     FragmentManager mFragmentManager;
 
@@ -33,51 +28,32 @@ public class RootActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            switch_fragment(Fragments.HOME);
-        } else if (id == R.id.nav_drive) {
-            Intent intent = new Intent(this, NewDriveActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_stats) {
-            switch_fragment(Fragments.STATS);
-        } else if (id == R.id.nav_history) {
-            switch_fragment(Fragments.HISTORY);
-        } else if (id == R.id.nav_settings) {
-
+        // switch to fragment based on the ID
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                switch_fragment(Fragments.HOME);
+                break;
+            case R.id.nav_stats:
+                switch_fragment(Fragments.STATS);
+                break;
+            case R.id.nav_history:
+                switch_fragment(Fragments.HISTORY);
         }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
-    public enum Fragments {HOME, STATS, HISTORY};
+    public enum Fragments {HOME, STATS, HISTORY}
 
+    // allows children fragment to change the current fragment
     public void switch_fragment(Fragments fragment) {
         FragmentTransaction ft = mFragmentManager.beginTransaction();
         switch(fragment) {
